ef4248e4bf89921a0ef360e328a40394201b684c
kaiyan910 <kaiyan@crookk.com>
2023-10-02 17:35:20 +0800

    added mantine logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Container, Flex, Text, Image } from '@mantine/core';
 import react from './assets/react.svg';
 import vite from './assets/vite.svg';
+import mantine from './assets/mantine.svg';
 
 function App() {
   return (
     <Container size="md" h="100vh" fluid>
       <Flex justify="center" align="center" h="100%" direction="column" gap="xl">
         <Flex justify="center" align="center" direction="row" gap="md">
-          <Image width="5rem" mx="auto" src={react} alt="Logo of React.JS" />
-          <Image width="5rem" mx="auto" src={vite} alt="Logo of Vite.JS" />
+          <Image width="5rem" src={vite} alt="Logo of Vite.JS" />
+          <Image width="5rem" src={react} alt="Logo of React.JS" />
+          <Image width="5rem" src={mantine} alt="Logo of Mantine" />
         </Flex>
         <Text
           variant="gradient"
