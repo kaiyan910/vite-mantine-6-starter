d551f1e23c7f99c42cbf0342a861481e5bfd038a
kaiyan910 <kaiyan@crookk.com>
2023-10-02 17:29:08 +0800

    added mantine code for example

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import './App.css';
+import { Container, Flex, Text, Image } from '@mantine/core';
+import react from './assets/react.svg';
+import vite from './assets/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Container size="md" h="100vh" fluid>
+      <Flex justify="center" align="center" h="100%" direction="column" gap="xl">
+        <Flex justify="center" align="center" direction="row" gap="md">
+          <Image width="5rem" mx="auto" src={react} alt="Logo of React.JS" />
+          <Image width="5rem" mx="auto" src={vite} alt="Logo of Vite.JS" />
+        </Flex>
+        <Text
+          variant="gradient"
+          gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+          sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+          ta="center"
+          fz="xl"
+          fw={700}
+        >
+          Template for Vite + React + Mantine v6
+        </Text>
+      </Flex>
+    </Container>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { MantineProvider } from '@mantine/core';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
